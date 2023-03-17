ade8b24813e23605490b7d36d3b0223829071574
clem-fry <98823205+clem-fry@users.noreply.github.com>
2023-03-17 21:45:49 +0000

    Update Data_clean.m
    Adding weather variables to the data

--- Data_clean.m
+++ Data_clean.m
@@ -11,7 +11,7 @@ sample.Month = sample_month;
 sample.Day = sample_day;
 %sample.Hour = sample_hour;
 sample.Time = (sample_hour + (sample_minute/60)); % this is the hour and minutes as fraction
-sample.pickup_datetime = [];
+%hideRequirementColumn(sample, "pickup_datetime");
 sample.dropoff_datetime = [];
 
 not_stored = sample.store_and_fwd_flag == "N";
@@ -19,4 +19,17 @@ sample.store_and_fwd_flag = double(not_stored); % converts to binary result
 
 sample.Distance = deg2km(distance(sample.pickup_latitude, sample.pickup_longitude, sample.dropoff_latitude, sample.dropoff_longitude)); % takes account of curvature
 
-sample.days_of_week_onehot = dummyvar(weekday(date))
+sample.days_of_week_onehot = dummyvar(weekday(date));
+
+%outliers, save sample, weather
+
+W = readtable('weather_data_nyc.csv');
+%weather_date = datetime(W.date, 'InputFormat', 'd-MM-yyy');
+
+sample.comparable_date = day(datetime(sample.pickup_datetime, 'InputFormat', 'yyyy-MM-dd'), 'dayofyear');
+W.comparable_date = day(datetime(W.date, 'InputFormat', 'dd-MM-yy'), 'dayofyear');
+sample = join(sample, W, 'Keys', 'comparable_date');
+sample.comparable_date = [];
+sample.pickup_datetime = [];
+
+sample = rmmissing(sample);
