fca985e395251f8f92ac50b0cf3780ec062b5c3d
MilesWeedon <98825066+MilesWeedon@users.noreply.github.com>
2023-03-25 19:51:13 +0000

    Update Pickup_against_time.m
    Now includes a density subplot. Other features such as legends also included

--- Pickup_against_time.m
+++ Pickup_against_time.m
@@ -1,4 +1,5 @@
 clear all; close all; clc;
+warning off;
 
 %% Import sample data
 Data_clean;
@@ -38,23 +39,42 @@ for i = 0:23
     Idx = find(sample.Hour == i);
     lat = sample.pickup_latitude(Idx);
     lon = sample.pickup_longitude(Idx);
-
     total = [total numel(lat)];
 
-    geoscatter(lat, lon, 'filled');
-
+    %First subplot
+    subplot(1,2,1);
+    geoscatter(lat, lon, 'filled', 'LineWidth',1);
     hold on;
     geobasemap('satellite');
-    geolimits([lat_min lat_max],[lon_min lon_max]);
-
+    %geolimits([lat_min lat_max],[lon_min lon_max]);
+    %geolimits([40.69 40.8],[-74.05 -73.9]);
+    geolimits([40.59 40.9],[-74.15 -73.8]);
     hold off;
-    title_string = ["All taxi pickups from sample, between: " times(i+1)];
-    title(title_string, 'FontSize', 15);
+    legend('Taxi pickup points', 'FontSize', 15);
+    title('All taxi pickups from sample, across the current hour', 'FontSize', 15);
 
+    %Second subplot
+    subplot(1,2,2);
+    geodensityplot(lat, lon,'FaceColor','interp');
+    hold on;
+    geobasemap('satellite');
+    %geolimits([lat_min lat_max],[lon_min lon_max]);
+    %geolimits([40.69 40.8],[-74.05 -73.9]);
+    geolimits([40.59 40.9],[-74.15 -73.8]);
+    hold off;
+    legend('Pickup density', 'FontSize', 15);
+    title('The density of taxi pickups from sample, across the current hour', 'FontSize', 15);
+
+    %Plot title
+    title_string = ["Taxi sample data between: " times(i+1)];
+    sgtitle(title_string, 'FontSize', 25);
+
+    %Add current frame to video
     f = getframe(gcf);
     writeVideo(obj,f);
 
-    pause(0.5);
+
+    pause(0.1);
 end
 
 obj.close();
@@ -65,6 +85,7 @@ obj.close();
 hour = 1:24;
 figure('units','normalized','outerposition',[0 0 1 1]);
 bar(hour, total);
+xlim([0.35 24.65]);
 xlabel('Hour of day (ranging from the first hour to the twenty-fourth hour)');
 ylabel('Total number of pickups');
 title('Total number of pickups from sample dataset, during each hour', 'FontSize', 15);
