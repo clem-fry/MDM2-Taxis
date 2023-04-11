652dfb2eb398d5c0a94f337098ec6feb7e9c3dd0
MaxIsSwagg <98823226+MaxIsSwagg@users.noreply.github.com>
2023-04-11 18:21:41 +0100

    Add files via upload

--- MDM2fictree.m
+++ MDM2fictree.m
@@ -1,7 +1,15 @@
 %Fictree coder
+
+%Data can be changed to analyze different components
 load 'sample';
+T2 = readtable('test_cleaned.csv');
 
 Xtrain = sample.passenger_count;
 Ytrain = sample.Time;
+Xtest = T2.passenger_count;
+
 tree = fitctree(Xtrain, Ytrain);
-view(tree,'mode','graph')
+view(tree,'mode','graph');
+
+YPred = predict(tree, Xtest);
+scatter(Xtest,YPred)
