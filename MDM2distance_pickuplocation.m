5187a9df2854918b80b54b96a41e2fd14e71b5a3
MaxIsSwagg <98823226+MaxIsSwagg@users.noreply.github.com>
2023-04-18 13:31:17 +0100

    Add files via upload

---
+++ MDM2distance_pickuplocation.m
@@ -0,0 +1,16 @@
+clear all
+load 'sample';
+T2 = readtable('test_cleaned.csv');
+
+X1train = sample.pickup_longitude;
+X2train = sample.pickup_latitude;
+Ytrain = sample.Distance;
+tree = fitrtree([X1train, X2train], Ytrain);
+X1test = T2.pickup_longitude;
+X2test = T2.pickup_latitude;
+Y_pred = predict(tree, [X1test,X2test]);
+fig1 = figure(1);
+scatter3(X1test,X2test,Y_pred)
+xlabel('Longitude')
+ylabel('Latitude')
+zlabel('Distance')
